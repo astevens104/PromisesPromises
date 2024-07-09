ce1bd72edc8c3ce1ecec9b013e4908c749fa66ee
astevens104 <agstevens104@gmail.com>
2024-07-09 14:28:26 -0700

    final promises

--- app.js
+++ app.js
@@ -1,62 +1,51 @@
-// fetch("https://jsonplaceholder.typicode.com/users/1")
-// const emailRef =document.querySelector(".email")
-// console.log(emailRef)
+const statusRef = document.querySelector(".status");
+const videoRef = document.querySelector(".video")
 
-// 1. Then
-
-//  fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
-//      return response.json()
-//  }).then(data => {
-//          emailRef.innerHTML = data.email
-//      });
+ function getSubscriptionStatus() {
+     return new Promise((resolve, reject) => {
+         resolve("FRE");
+    }); 
+ }
 
 
-// Order is different because of the amount of time required to execute the js.  Therefore, the console.log(3) will execute before the console.log(2 has finished doing it's thing)
-  
 
-//response.json() is converting something we got from the backend into something compatible w/ the frontend
+// The Challenge:
 
-// 2. Async/Await
+// 1. Create a function called `getVideo`
+// 2. Accept a parameter called `subscriptionStatus
+// 3. Return a new promise inside the function that:
+//     - if `VIP, resolve "show video"
+//     - if `FREE` resolve "show trailer"
+//     - otherwise reject "no video"
+// 4. console.log the result of getVideo(status) in main()
 
-// async function main() {
-// 
-//  await REQUIRES an async function to operate properly
-//   await needs to be INSIDE an async function
-//  async/await returns the response for us
-//     const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+// The solution
 
-//   we unlocked the information by using await and storing the information in a variable ('data')  
-//     const data = await response.json() {
- 
-//     emailRef.innerHTML = data.email
-// }
-// // the console.log (1,2,3) shows us the code is being executed in order
-//main()
-
-
-// Main takeaway: Then runs in the background
-// Async/await runs line by line
-
-// How to create a promise:
-
-//If promise is succesful, we resolve
-// otherwise, we reject
-
-const statusRef = document.querySelector('.status')
-
-function getSubscriptionStatus() {
+function getVideo(subscriptionStatus) {
     return new Promise((resolve, reject) => {
-        resolve("VIP")
-    })  
+        if (subscriptionStatus === "VIP") {
+            resolve("show video")
+        }
+        else if (subscriptionStatus === "FREE") {
+            resolve("show trailer")
+        }
+        else {
+            reject("no video")
+        }    
+    })
 }
 
-// 1. Then
-//  getSubscriptionStatus().then(response => console.log(response))
 
-// 2. Async/Await
 async function main() {
-    const status = (await getSubscriptionStatus())
-    statusRef.innerHTML = status
-}
-
+     const status = await getSubscriptionStatus();
+     statusRef.innerHTML = status;
+     try {
+     console.log(await getVideo(status))
+     }
+     catch (e) {
+        console.log(e)
+        videoRef.innerHTML = e;
+     }
+ }
+    
 main();
