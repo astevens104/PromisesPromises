449bda121b1d1432260de41318f205e596be12bd
astevens104 <agstevens104@gmail.com>
2024-07-09 12:05:40 -0700

    more promises

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // fetch("https://jsonplaceholder.typicode.com/users/1")
-const emailRef =document.querySelector(".email")
+// const emailRef =document.querySelector(".email")
 // console.log(emailRef)
 
 // 1. Then
@@ -18,21 +18,45 @@ const emailRef =document.querySelector(".email")
 
 // 2. Async/Await
 
- async function main() {
-     console.log(1)
+// async function main() {
+// 
 //  await REQUIRES an async function to operate properly
 //   await needs to be INSIDE an async function
 //  async/await returns the response for us
-     const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
 
 //   we unlocked the information by using await and storing the information in a variable ('data')  
-     const data = await response.json()
+//     const data = await response.json() {
  
-     emailRef.innerHTML = data.email
-}
+//     emailRef.innerHTML = data.email
+// }
 // // the console.log (1,2,3) shows us the code is being executed in order
-main()
+//main()
 
 
 // Main takeaway: Then runs in the background
 // Async/await runs line by line
+
+// How to create a promise:
+
+//If promise is succesful, we resolve
+// otherwise, we reject
+
+const statusRef = document.querySelector('.status')
+
+function getSubscriptionStatus() {
+    return new Promise((resolve, reject) => {
+        resolve("VIP")
+    })  
+}
+
+// 1. Then
+//  getSubscriptionStatus().then(response => console.log(response))
+
+// 2. Async/Await
+async function main() {
+    const status = (await getSubscriptionStatus())
+    statusRef.innerHTML = status
+}
+
+main();
